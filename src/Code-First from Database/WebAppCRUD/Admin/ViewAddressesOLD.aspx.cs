89a52cc7004396cfc178b544a4cfc57f5861cae2
rball11 <46543426+rball11@users.noreply.github.com>
2019-09-24 16:51:32 -0600

    Re #1 - Added web form w/ Master Page

--- src/Code-First from Database/WebAppCRUD/Admin/ViewAddressesOLD.aspx.cs
+++ src/Code-First from Database/WebAppCRUD/Admin/ViewAddressesOLD.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebAppCRUD.Admin
 {
-    public partial class ViewAddresses1 : System.Web.UI.Page
+    public partial class ViewAddresses : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
