c7089bad8d03693491a8f4ae18885a21f211c49c
rball11 <46543426+rball11@users.noreply.github.com>
2019-09-24 15:56:09 -0600

    Add Supplier Data Souce Validation on Inserted

--- src/Code-First from Database/WebAppCRUD/Admin/ViewSuppliers.aspx.cs
+++ src/Code-First from Database/WebAppCRUD/Admin/ViewSuppliers.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,59 @@ namespace WebAppCRUD.Admin
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void SuppliersListView_ItemInserting(object sender, ListViewInsertEventArgs e)
+        {
+            //this fires just before the ListView calls the Object Data Source control to do an insert.
+            ; //no operations
+        }
+
+        protected void SuppliersListView_ItemInserted(object sender, ListViewInsertedEventArgs e)
+        {
+            //this event is fired after the Object Data Source has returned from performing an insert
+            ;
+        }
+
+        protected void SuppliersDataSource_Inserting(object sender, ObjectDataSourceMethodEventArgs e)
+        {
+            ; //Before calling the BLL
+        }
+
+        protected void SuppliersDataSource_Inserted(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            ; //after the BLL call
+            if (e.Exception != null)
+            {
+                Exception inner = e.Exception;
+                while(inner.InnerException != null)
+                {
+                    inner = inner.InnerException;
+                }
+
+                string message = $"Problem inserting: {inner.GetType().Name } <blockquote>{ inner.Message }</blockquote>";
+
+
+                if (inner is DbEntityValidationException)
+                {
+                    //Safe Type-Cast
+                    var actual = inner as DbEntityValidationException;
+                    message += "<ul>";
+                    foreach(var detail in actual.EntityValidationErrors)
+                    {
+                        message += $"<li>{detail.Entry.Entity.GetType().Name}";
+                        message += "<ol>";
+                        foreach (var error in detail.ValidationErrors)
+                        {
+                            message += $"<li>{error.ErrorMessage}</li>";
+                        }
+                        message += "</ol></li>";
+                    }
+                }
+
+                MessageLabel.Text = message;
+                e.ExceptionHandled = true;
+            }
         }
     }
 }
