6d9428e14421c921ad13a581a1d21a74f274a33c
rball11 <46543426+rball11@users.noreply.github.com>
2019-11-05 16:32:41 -0700

    Replace Hard-Coded Values

--- src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -7,8 +7,9 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using WebApp.Models;
+using static WebApp.Admin.Security.Settings;
 
-//you can learn about database initialization strategies in EF6 via
+// You can learn about Database Initialization Strategies in EF6 via
 // http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
 
 namespace WebApp.Admin.Security
@@ -17,47 +18,50 @@ namespace WebApp.Admin.Security
     /// Provide functionality for setting up the database for the ApplicationDbContext.
     /// The specific functionality is to create the database if it does not exist.
     /// </summary>
-    public class SecurityDbContextInitializer :  CreateDatabaseIfNotExists<ApplicationDbContext>
+    public class SecurityDbContextInitializer
+        : CreateDatabaseIfNotExists<ApplicationDbContext>
     {
         protected override void Seed(ApplicationDbContext context)
         {
-            //To "seed" a database is to provide it with some initial data when the database is created
+            // To "seed" a database is to provide it with some initial data
+            // when the database is created.
 
             #region Seed the security roles
-            //make the Identity's BLL instance to help us manage roles
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context)); //the RoleManager<T> and RoleStore<T> are BLL classes that give flexibility
-                                                                                                   //to the design/structure of how we're using ASP.Net identity.
-                                                                                                   //The IdentityRole is an Entity class that represents a security role.
-            //TODO: Move these hard-coded security roles to Web.Config
-            roleManager.Create(new IdentityRole { Name = "Administrators" });
-            roleManager.Create(new IdentityRole { Name = "Registered Users" });
+            // Make the Identity's BLL instance to help us manage roles
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            // The RoleManager<T> and RoleStore<T> are BLL classes that give flexibility
+            // to the design/structure of how we're using Asp.NET Identity.
+            // The IdentityRole is an Entity class that represents a security role.
+
+            foreach (var role in DefaultSecurityRoles)
+                roleManager.Create(new IdentityRole { Name = role });
             #endregion
 
-            #region Seed the user
-            //Create an Admin user
+            #region Seed the users
+            // Create an Admin user
             var adminUser = new ApplicationUser
             {
-                //TODO: Move hard-coded values to Web.config
-                UserName = "WebAdmin",
-                Email = "Elections2020@Hackers.ru",
+                UserName = AdminUserName,
+                Email = AdminEmail,
                 EmailConfirmed = true
             };
-            //Instantiate the BLL user manager
+
+            // Instantiate the BLL user manager
             var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-            //the ApplicationUserManager is a BLL class in this website's App_Start/IdentityConfig.cs
-            var result = userManager.Create(adminUser, "Pa$$w0rd"); //TODO: Move password
+            // - The ApplicationUserManager is a BLL class in this website's App_Start/IdentityConfig.cs
+            var result = userManager.Create(adminUser, AdminPassword);
             if (result.Succeeded)
             {
-                //Get the ID that was generated for the user we just created/added
-                var adminId = userManager.FindByName("WebAdmin").Id;
-                //Add the user to the Administrators role
-                userManager.AddToRole(adminId, "Administrators");
+                // Get the Id that was generated for the user we created/added
+                var adminId = userManager.FindByName(AdminUserName).Id;
+                // Add the user to the Administrators role
+                userManager.AddToRole(adminId, AdminRole);
             }
 
-            //Create the other user accounts for all the people in my Demo database
+            // Create the other user accounts for all the people in my Demo database
             var demoManager = new DemoController();
             var people = demoManager.ListPeople();
-            foreach(var person in people)
+            foreach (var person in people)
             {
                 var user = new ApplicationUser
                 {
@@ -66,15 +70,16 @@ namespace WebApp.Admin.Security
                     EmailConfirmed = true,
                     PersonId = person.PersonID
                 };
-                result = userManager.Create(user, "Pa$$word1");
+                result = userManager.Create(user, TempPassword);
                 if (result.Succeeded)
                 {
                     var userId = userManager.FindByName(user.UserName).Id;
-                    userManager.AddToRole(userId, "Registered Users");
+                    userManager.AddToRole(userId, UserRole);
                 }
             }
             #endregion
-            //Note: keep this call to the base class so it can do its seeding work
+
+            // Note: Keep this call to the base class so it can do its seeding work
             base.Seed(context);
         }
     }
