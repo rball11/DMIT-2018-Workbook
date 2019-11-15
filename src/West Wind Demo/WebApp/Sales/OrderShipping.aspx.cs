2feb9b66f9bbea8cf639a625146819cb0f6bc306
rball11 <46543426+rball11@users.noreply.github.com>
2019-11-15 16:57:04 -0700

    Update Order Shipping

--- src/West Wind Demo/WebApp/Sales/OrderShipping.aspx.cs
+++ src/West Wind Demo/WebApp/Sales/OrderShipping.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApp.Admin.Security; // For the Settings class
 
 namespace WebApp.Sales
 {
@@ -11,7 +12,15 @@ namespace WebApp.Sales
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Request.IsAuthenticated || !User.IsInRole(Settings.SupplierRole))
+                Response.Redirect("~", true);
+            if (!IsPostBack)
+            {
+                // Load up the info on the supplier
+                // TODO: Replace hard-coded supplier ID with the user's supplier ID
+                SupplierInfo.Text = "Temp supplier: ID 2";
 
+            }
         }
     }
 }
